16fbb34bb084bda5a9372b21f910ae27be3f9e3d
oleg fruntov <fruntovoleg@gmail.com>
2023-05-19 12:48:15 +0300

    Add project files.

---
+++ HtmlToPdf/Program.cs
@@ -0,0 +1,29 @@
+﻿using iText.Html2pdf;
+
+namespace HtmlToPdf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using var file = new FileStream("baseTemplateToHtml.html", FileMode.Open);
+
+            using var reader = new StreamReader(file);
+
+            string html = reader.ReadToEnd();
+
+            ConvertToPdf(html);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Done");
+            Console.ResetColor();
+        }
+
+        static void ConvertToPdf(string html)
+        {
+            using var file = new FileStream("output.pdf", FileMode.Create);
+
+            HtmlConverter.ConvertToPdf(html, file);
+        }
+    }
+}
